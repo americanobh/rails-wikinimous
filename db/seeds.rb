710caedf52695b0160469ef01e22eb13a81e155a
Felipe Americano <felip.americano@gmail.com>
2020-04-18 16:25:26 -0300

    usar sempre destroy

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+20.times do
+  Article.create(
+    title: Faker::Restaurant.name,
+    content: Faker::Lorem.paragraph_by_chars(number: 5000, supplemental: false)
+  )
+end
